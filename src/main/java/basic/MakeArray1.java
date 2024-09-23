fce1ce8bc3a54721c13ee1b84b0d1b69728e7088
cw_hyj <yj.hwang@codewise.co.kr>
2024-09-23 20:34:15 +0900

    #basic 배열 만들기

---
+++ src/main/java/basic/MakeArray1.java
@@ -0,0 +1,49 @@
+package basic;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class MakeArray1 {
+
+    //문제
+//    class Solution {
+//        public int[] solution(int n, int k) {
+//            int[] answer = {};
+//            return answer;
+//        }
+//    }
+
+//정수 n과 k가 주어졌을 때,
+// 1 이상 n이하의 정수 중에서 k의 배수를 오름차순으로 저장한 배열을 return 하는 solution 함수를 완성해 주세요.
+
+   //ex int = 10 , int k = 3 , result= [3,6,9]
+
+
+    //설명
+
+    public static void main(String[] args) {
+
+        int n =10;
+        int k=3;
+
+        MakeArray1 solutuin  = new MakeArray1();
+
+        System.out.println("solutuin = " + Arrays.toString(solutuin.soulution1(n,k)));
+        System.out.println("solutuin = " + Arrays.toString(solutuin.soulution2(n,k)));
+
+    }
+
+    public int[] soulution1(int n ,int k){
+        int[] answer = new int[n/k];
+        for (int i = 1; i <= n/k; i++){
+            answer[i -1] = k *i;
+        }
+        return answer;
+    }
+
+
+    public int[] soulution2(int n ,int k){
+
+        return IntStream.rangeClosed(1,n).filter(i -> i % k == 0).toArray();
+    }
+}
